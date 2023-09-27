d61823ef53eb6a3dd7bb996dd58dc0a40be04ab8
Григорий Петров <petrovgrigorij29@gmail.com>
2023-09-27 18:36:12 +0400

    second commit(reverse array, add headers)

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests
-data="2023/09/09"
+data="2023/09/26"
+dates=[]
 with open ("dataset.csv", "w") as f:
     while data!="2018/12/30":
-        response= requests.get(f"https://www.cbr-xml-daily.ru/archive/{data}/daily_json.js")
-        f.write(f'{data} , {response.json()["Valute"]["USD"]["Value"]}\n')
+        response= requests.get(f"https://www.cbr-xml-daily.ru/archive/{data}/daily_json.js", headers={"User-Agent":"Mozilla/5.0"})
+        dates.append(f'{data},{response.json()["Valute"]["USD"]["Value"]}')
         data=response.json()["PreviousDate"][:10].replace("-", "/")
+    dates = reversed(dates)
+    for date in dates:
+        f.write(f"{date}\n")
 
